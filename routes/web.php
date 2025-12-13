9f80f0a6da055852cd99e612ff13689ef286f890
FRANCISCO-ATEMPA <fcoatempaa@gmail.com>
2025-12-13 02:26:00 -0600

    Sexto commit

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,50 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->user();
+
+    $user = User::updateOrCreate(
+        ['google_id' => $user_google->id],
+        [
+            'name'  => $user_google->name,
+            'email' => $user_google->email,
+            'avatar'=> $user_google->avatar,
+        ]
+    );
+
+    Auth::login($user);
+
+    return redirect('/dashboard');
+
+    // $user->token
+});
+
+
+
+Route::get('google-auth/redirect', function(){
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('google-auth/callback', function(){
+    $user = Socialite::driver('google')->user();
+
+    // $user->token
+});
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
